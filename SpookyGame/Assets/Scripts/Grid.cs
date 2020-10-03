0405ec93e362bc208e0eb72fcc2c53f58f1066ad
Nick Rasmussen <nlr30262g.rit.edu>
2020-10-03 03:53:41 -0400

    level generation inching closer

--- SpookyGame/Assets/Scripts/Grid.cs
+++ SpookyGame/Assets/Scripts/Grid.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.IO;
 
 public class Grid : MonoBehaviour
 {
@@ -11,6 +12,7 @@ public class Grid : MonoBehaviour
     [SerializeField] GameObject pref_GridTile;
 
     public GameObject[,] grid;
+    int[,] grid_modules;
 
     // Start is called before the first frame update
     void Start()
@@ -25,11 +27,72 @@ public class Grid : MonoBehaviour
                 tile.transform.parent = gameObject.transform;
             }
         }
+
+        GenerateLevel();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+
+
+
+    //level generation stuff oh boy here we go
+    void GenerateLevel()
+    {
+        //TODO: change this to a 3D array
+        string[] module0 = File.ReadAllLines("Assets/Resources/modules_type0.txt");
+        string[] module1 = File.ReadAllLines("Assets/Resources/modules_type1.txt");
+        string[] module2 = File.ReadAllLines("Assets/Resources/modules_type2.txt");
+
+        grid_modules = new int[4, 4];
+        for(int r = 0; r < grid_modules.GetLength(0); r++)
+        {
+            for(int c = 0; c < grid_modules.GetLength(1); c++)
+            {
+                int moduleType = Random.Range(0, 3);
+                grid_modules[c, r] = moduleType;
+
+                switch (moduleType)
+                {
+                    case 0:
+                        FillGridWithModule(c * 10, r * 8, module0);
+                        break;
+                    case 1:
+                        FillGridWithModule(c * 10, r * 8, module1);
+                        break;
+                    case 2:
+                        FillGridWithModule(c * 10, r * 8, module2);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+
+    void FillGridWithModule(int pX, int pY, string[] module)
+    {
+        for(int r = 0; r < module.Length; r++)
+        {
+            for(int c = 0; c < module[r].Length; c++)
+            {
+                char tileType = module[r][c];
+                switch (tileType)
+                {
+                    case '#':
+                        grid[pX + c, pY + r].GetComponent<GridObj>().SetState(1);
+                        break;
+                    case '.':
+                        grid[pX + c, pY + r].GetComponent<GridObj>().SetState(0);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
     }
 }
