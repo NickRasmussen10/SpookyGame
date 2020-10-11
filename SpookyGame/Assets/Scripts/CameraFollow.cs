167a06db6a0ac5ff23fe5ee3c09e853efaac132d
Nick Rasmussen <nlr30262g.rit.edu>
2020-10-11 16:22:24 -0400

    gave camera follow a deadzone

--- SpookyGame/Assets/Scripts/CameraFollow.cs
+++ SpookyGame/Assets/Scripts/CameraFollow.cs
@@ -7,6 +7,7 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] Transform followObj;
     float zPos = 0;
 
+    [SerializeField] Vector2 deadZone;
 
     // Start is called before the first frame update
     void Start()
@@ -14,13 +15,28 @@ public class CameraFollow : MonoBehaviour
         zPos = transform.position.z;
 
         if (!followObj) followObj = FindObjectOfType<Player>().gameObject.transform;
+
+        Vector3 startPos = followObj.position;
+        startPos.z = zPos;
+        transform.position = startPos;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 newPos = followObj.position;
-        newPos.z = zPos;
+        Vector3 newPos = transform.position;
+        Vector3 camToFollow = followObj.position - transform.position;
+
+        //deadzone stuff, not gonna lie I don't really know why it works I just messed with the numbers until it did
+        if (camToFollow.x > deadZone.x) newPos.x += followObj.position.x - transform.position.x - deadZone.x;
+        else if (camToFollow.x < -deadZone.x) newPos.x += followObj.position.x - transform.position.x + deadZone.x;
+
+        if (camToFollow.y > deadZone.y) newPos.y += followObj.position.y - transform.position.y - deadZone.y;
+        else if (camToFollow.y < -deadZone.y) newPos.y += followObj.position.y - transform.position.y + deadZone.y;
+
         transform.position = newPos;
+        //Vector3 newPos = followObj.position;
+        //newPos.z = zPos;
+        //transform.position = newPos;
     }
 }
